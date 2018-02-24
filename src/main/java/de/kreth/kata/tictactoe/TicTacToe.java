92600358484a497a5b5db98dc289e9e9d0370162
Markus Kreth <markus.kreth@web.de>
2018-02-24 14:21:45 +0100

    switch to swing gui

--- src/main/java/de/kreth/kata/tictactoe/TicTacToe.java
+++ src/main/java/de/kreth/kata/tictactoe/TicTacToe.java
@@ -1,9 +1,12 @@
 package de.kreth.kata.tictactoe;
 
+import de.kreth.kata.tictactoe.ui.console.ConsoleBoard;
+import de.kreth.kata.tictactoe.ui.console.ConsoleInputReader;
+
 public class TicTacToe {
 
 	public static void main(String[] args) {
-		new Game().start();
+		new Game(new ConsoleBoard(System.out), new ConsoleInputReader()).start();
 	}
 
 }
